9141397c456ed7345dc98af7b385e2510aca5a95
Joe O'Leary (Junior) <180karma@gmail.com>
2025-10-15 05:05:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3721884746-ec8c3",
+  "appId": "1:463052960747:web:201e483bdd7f4e6b9adfae",
+  "apiKey": "AIzaSyAcbsEm1VDHqwqSpzCr0fe4IgQ4xzxQ0xg",
+  "authDomain": "studio-3721884746-ec8c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "463052960747"
+};
